cf8a6066efda01ba83565dd5d472183943ef9612
mikep996 <54072448+mikep996@users.noreply.github.com>
2024-11-29 17:00:52 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,6 @@
 call d:\Aldec\Riviera-PRO-2024.04-x64\etc\setenv.bat 
 set VUNIT_SIMULATOR=rivierapro
-set CONTRIBUTION=0
 vsim -version
 python run.py %1 %2 %3 %4 %5
-rem vsim -c -do "do src/runme.do; quit"
+vsim -c -do "do src/runme.do; quit"
 rem pause
